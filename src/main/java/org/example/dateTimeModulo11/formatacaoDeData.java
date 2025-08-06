8475d1c125ae28eafbcf5f492f7af72746f5286b
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-05 21:35:24 -0300

    feat: como formatar data

---
+++ src/main/java/org/example/dateTimeModulo11/formatacaoDeData.java
@@ -0,0 +1,28 @@
+package org.example.dateTimeModulo11;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class formatacaoDeData {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2026-02-13");
+        LocalDateTime d05 = LocalDateTime.parse("2026-02-13T00:00:00");
+        Instant d06 = Instant.parse("2026-02-13T00:00:00Z");
+
+        // formatar objeto data para customizado
+        // necessita de formatador primeiro
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println(d04.format(fmt1));
+        // OU
+        System.out.println(fmt1.format(d04));
+
+        // formatar data-hora para customizado
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        System.out.println(fmt2.format(d05));
+        // OU
+        System.out.println(d05.format(fmt2));
+    }
+}
