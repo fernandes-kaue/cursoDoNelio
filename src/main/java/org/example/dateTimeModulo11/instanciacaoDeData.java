1f2b222b519b50cb9498747904aad82b7f042619
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-05 21:23:24 -0300

    init: initial commit

---
+++ src/main/java/org/example/dateTimeModulo11/instanciacaoDeData.java
@@ -0,0 +1,57 @@
+package org.example.dateTimeModulo11;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class instanciacaoDeData {
+    public static void main(String[] args) {
+        // cria data do dia atual
+        LocalDate d01 =  LocalDate.now();
+        System.out.println(d01);
+
+        // cria data com horário e fuso
+        LocalDateTime d02 = LocalDateTime.now();
+        System.out.println(d02);
+
+        // cria data com horário e fuso com o Z no final (GMT)
+        Instant d03 = Instant.now();
+        System.out.println(d03);
+
+        // instancia data a partir de texto ISO 8601
+        LocalDate d04 = LocalDate.parse("2026-02-13");
+        System.out.println(d04);
+
+        // instancia data a partir de texto ISO 8601 COM HORARIO
+        LocalDateTime d05 = LocalDateTime.parse("2026-02-13T00:00:00");
+        System.out.println(d05);
+
+        // instancia data a partir de texto ISO 8601 COM FUSO GMT
+        Instant d06 = Instant.parse("2026-02-13T00:00:00Z");
+        System.out.println(d06);
+
+        // instancia data a partir de texto ISO 8601 COM FUSO
+        // diferente do GMT
+        Instant d07 = Instant.parse("2026-02-13T00:00:00-03:00");
+        System.out.println(d07);
+
+        // instanciação de data a partir de texto em formato customizado
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate d08 = LocalDate.parse("13/06/2029", fmt1);
+        System.out.println(d08);
+
+        // instanciação de data hora a partir de texto em formato customizado
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime d09 = LocalDateTime.parse("13/06/2029 12:02", fmt2);
+        System.out.println(d09);
+
+        // instanciação de dados isolados
+        LocalDate d10 = LocalDate.of(2025, 12, 5);
+        System.out.println(d10);
+
+        LocalDateTime d11 = LocalDateTime.of(2025, 12, 5, 12, 35);
+        System.out.println(d11);
+    }
+}
+
