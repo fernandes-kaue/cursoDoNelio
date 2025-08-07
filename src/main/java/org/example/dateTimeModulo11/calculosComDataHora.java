f3d339534eb2762843c2c3826b9c89097ab95c47
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-06 23:25:56 -0300

    docs: observação sobre cálculos

--- src/main/java/org/example/dateTimeModulo11/calculosComDataHora.java
+++ src/main/java/org/example/dateTimeModulo11/calculosComDataHora.java
@@ -2,6 +2,16 @@ package org.example.dateTimeModulo11;
 
 // quick reminder:
 // data hora são objetos imutáveis
+// para fazer calculos deve-se
+// criar um novo objeto com a data resultante
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class calculosComDataHora {
+    LocalDate d04 = LocalDate.parse("2026-02-13");
+    LocalDateTime d05 = LocalDateTime.parse("2026-02-13T00:00:00");
+    Instant d06 = Instant.parse("2026-02-13T00:00:00Z");
+
 }
