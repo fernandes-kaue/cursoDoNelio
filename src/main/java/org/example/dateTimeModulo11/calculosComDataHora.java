32acc4a263f8b0201ee24f039c481e0ce1bc233c
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-07 21:29:54 -0300

    feat: calculo de duração + metodo main

--- src/main/java/org/example/dateTimeModulo11/calculosComDataHora.java
+++ src/main/java/org/example/dateTimeModulo11/calculosComDataHora.java
@@ -5,45 +5,54 @@ package org.example.dateTimeModulo11;
 // para fazer calculos deve-se
 // criar um novo objeto com a data resultante
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class calculosComDataHora {
-    LocalDate d04 = LocalDate.parse("2026-02-13");
-    LocalDateTime d05 = LocalDateTime.parse("2026-02-13T00:00:00");
-    Instant d06 = Instant.parse("2026-02-13T00:00:00Z");
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2026-02-13");
+        LocalDateTime d05 = LocalDateTime.parse("2026-02-13T00:00:00");
+        Instant d06 = Instant.parse("2026-02-13T00:00:00Z");
 
 
-    // PARA LOCALDATE
-    // cria uma nova data com x dias a menos
-    LocalDate pastWeekLocalDate = d04.minusDays(7);
-    // cria uma nova data com x dias a mais
-    LocalDate nextWeekLocalDate = d04.plusDays(7);
+        // PARA LOCALDATE
+        // cria uma nova data com x dias a menos
+        LocalDate pastWeekLocalDate = d04.minusDays(7);
+        // cria uma nova data com x dias a mais
+        LocalDate nextWeekLocalDate = d04.plusDays(7);
 
-    //cria uam nova dat com x anos a menos
-    LocalDate pastYearLocalDate = d04.minusYears(1);
-    // cria uma nova data com x anos a mais
-    LocalDate nextYearLocalDate = d04.plusYears(1);
+        //cria uam nova dat com x anos a menos
+        LocalDate pastYearLocalDate = d04.minusYears(1);
+        // cria uma nova data com x anos a mais
+        LocalDate nextYearLocalDate = d04.plusYears(1);
 
-    // PARA LOCALDATETIME
-    // cria uma nova localdatetime com x horas a mais
-    // contraparte é minusHours();
-    LocalDateTime nextHourLocalDateTime = d05.plusHours(1);
-    // cria uma nova localdatetime com x minutos a mais
-    // contraparte é minusMinutes();
-    LocalDateTime nextFifteenLocalDateTime = d05.plusMinutes(15);
-    // cria uma nova localdatetime com x segundos a mais
-    // contraparte é minusSeconds();
-    LocalDateTime nextTenSecondsLocalDateTime = d05.plusSeconds(10);
+        // PARA LOCALDATETIME
+        // cria uma nova localdatetime com x horas a mais
+        // contraparte é minusHours();
+        LocalDateTime nextHourLocalDateTime = d05.plusHours(1);
+        // cria uma nova localdatetime com x minutos a mais
+        // contraparte é minusMinutes();
+        LocalDateTime nextFifteenLocalDateTime = d05.plusMinutes(15);
+        // cria uma nova localdatetime com x segundos a mais
+        // contraparte é minusSeconds();
+        LocalDateTime nextTenSecondsLocalDateTime = d05.plusSeconds(10);
 
-    // PARA INSTANT
-    // não tem métodos para dia/hora/etc
-    // necessita passar como parametro qual unidade temporal será
-    // adicionada ou subtraída
-    // essas unidades são advindas da classe ChronoUnit
-    Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
-    Instant nextWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        // PARA INSTANT
+        // não tem métodos para dia/hora/etc
+        // necessita passar como parametro qual unidade temporal será
+        // adicionada ou subtraída
+        // essas unidades são advindas da classe ChronoUnit
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+        Instant nextWeekInstant = d06.minus(7, ChronoUnit.DAYS);
 
+        // CALCULAR DURAÇÃO
+        // necessita da classe Duration
+        // só funciona com LocalDateTime e Instant
+        Duration t1 = Duration.between(pastWeekInstant, d06);
+        System.out.println(t1.toDays());
+    }
 }
