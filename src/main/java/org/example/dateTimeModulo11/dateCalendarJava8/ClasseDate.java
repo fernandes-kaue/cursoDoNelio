7c9a02147a6db09af3af75bb55e947fe773e9f59
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-09 22:20:13 -0300

    refactor: adição de SOUT para ler melhor

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseDate.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseDate.java
@@ -43,24 +43,34 @@ public class ClasseDate {
         Date y1 = sdf1.parse("09/08/2025");
         Date y2 = sdf2.parse("09/08/2025 21:21:21");
 
+        System.out.println("Print no formato padrão da classe Date: ");
         System.out.println("Y1: " + y1);
         System.out.println("Y2: " + y2);
+        System.out.println();
+
+        System.out.println("Print no formato passado da SimpleDateFormat: ");
 
         System.out.println("Y1: " + sdf1.format(y1));
         System.out.println("Y2: " + sdf2.format(y2));
+        System.out.println();
+
         /*
         Para criar Date a partir de um Instant
          */
+        System.out.println("Print de data instanciada a partir de um Instant: ");
         Date y3 = Date.from(Instant.parse("2026-01-01T00:00:00Z"));
         System.out.println("Y3: " + y3);
+        System.out.println();
 
         // para criar Date com horário atual:
+        System.out.println("Print de data instanciada a partir do horário atual: ");
         Date x1 = new Date();
         System.out.println("X1: " + x1);
 
         // ou
         Date x2 = new Date(System.currentTimeMillis());
         System.out.println("X2: " + x2);
+        System.out.println();
 
         /*
         Ao instanciar Date com 0L, é instanciado a data de
@@ -70,6 +80,7 @@ public class ClasseDate {
         exemplo:
         Date data = new Date(1000L (milisegundos) * 60L (vira minutos) * 60L (vira hora) * XL(vira x horas);
          */
+        System.out.println("Print de data instanciada a partir de passagem de Milissegundos: ");
         Date x3 = new Date(0L);
         System.out.println("X3: " + x3);
 
