f9d9fb7451d0c4f406a407bd1f4b0493b0236806
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-10 20:44:50 -0300

    feat: codigo base de Date

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseCalender.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseCalender.java
@@ -1,4 +1,17 @@
 package org.example.dateTimeModulo11.dateCalendarJava8;
 
+// Como manipular uma data usando o Calender
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
 public class ClasseCalender {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = Date.from(Instant.parse("2018-01-01T15:15:15Z"));
+
+        System.out.println(sdf.format(date));
+
+    }
 }
