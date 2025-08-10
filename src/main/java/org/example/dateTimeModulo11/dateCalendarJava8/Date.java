77f831bc72eacc0f7a0ee4f9d47892208f737159
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-09 21:29:01 -0300

    docs: o que é o Date

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/Date.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/Date.java
@@ -1,4 +1,10 @@
 package org.example.dateTimeModulo11.dateCalendarJava8;
 
+// Date repreasente um instante
+// é do pacote java.util
+//
+// O tipo Date armazena o número de milissegundos desde
+// 01/01/1970 GMT (UTC)
+
 public class Date {
 }
