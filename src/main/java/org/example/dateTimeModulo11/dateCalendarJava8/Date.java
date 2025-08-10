d5604514c22fd9e6b1cd17338f28e60ba2c148ab
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-09 21:32:09 -0300

    docs: o que é o SimpleDateFormat

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/Date.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/Date.java
@@ -6,5 +6,15 @@ package org.example.dateTimeModulo11.dateCalendarJava8;
 // O tipo Date armazena o número de milissegundos desde
 // 01/01/1970 GMT (UTC)
 
+// SimpleDateFormat
+// Define formatos para conversão entre Date e String
+// Ex:
+//
+// dd/MM/yyyy -> 09/08/2025
+// dd/MM/yyyy HH:mm:ss -> 09/08/2025 21:21:21
+
 public class Date {
+    public static void main(String[] args) {
+
+    }
 }
