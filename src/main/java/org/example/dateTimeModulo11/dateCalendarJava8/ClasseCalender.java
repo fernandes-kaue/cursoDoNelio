f21cb9176691cfc10894968e417a464d526de97a
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-10 20:55:07 -0300

    feat: como fazer calculos com Calendar

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseCalender.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseCalender.java
@@ -4,6 +4,7 @@ package org.example.dateTimeModulo11.dateCalendarJava8;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ClasseCalender {
@@ -11,7 +12,26 @@ public class ClasseCalender {
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = Date.from(Instant.parse("2018-01-01T15:15:15Z"));
 
-        System.out.println(sdf.format(date));
+        System.out.println("Date antes do cálculo: " + sdf.format(date));
+
+        // Para instanciarmos um Calendar:
+        Calendar cal = Calendar.getInstance();
+
+        // Para instanciar a Date dentro do Calender:
+        cal.setTime(date);
+
+        /*
+        Para adicionarmos X unidades de tempo na Date,
+        usamos:
+
+        cal.add(Calendar.{TIMEUNIT}, {QUANTIDADE});
+         */
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+
+        // para obter a data após o cálculo
+        date = cal.getTime();
+
+        System.out.println("Date após calculo: " + sdf.format(date));
 
     }
 }
