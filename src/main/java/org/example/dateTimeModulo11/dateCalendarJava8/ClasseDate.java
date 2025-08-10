79add724a015e5f7fa14d673e1140fa973026ef8
fernandes-kaue <90159661+fernandes-kaue@users.noreply.github.com>
2025-08-09 21:39:48 -0300

    feat: instanciação de Date a partir de SDF

--- src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseDate.java
+++ src/main/java/org/example/dateTimeModulo11/dateCalendarJava8/ClasseDate.java
@@ -13,12 +13,18 @@ package org.example.dateTimeModulo11.dateCalendarJava8;
 // dd/MM/yyyy -> 09/08/2025
 // dd/MM/yyyy HH:mm:ss -> 09/08/2025 21:21:21
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Date {
-    public static void main(String[] args) {
+public class ClasseDate {
+    public static void main(String[] args) throws ParseException {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Date y1 = sdf1.parse("09/08/2025");
+        Date y2 = sdf2.parse("09/08/2025 21:21:21");
 
     }
 }
