361fd86eaacfe816d378f5b8c185e09484a88df9
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-15 22:42:04 -0300

    docs(Composicao): informações básicas

---
+++ src/main/java/org/example/Modulo12EnumComposicao/Composicao.java
@@ -0,0 +1,19 @@
+package org.example.Modulo12EnumComposicao;
+
+public class Composicao {
+    public static void main(String[] args) {
+        /*
+        O que é composição?
+        Composição é um tipo de associação
+        que permite que um objeto contenha outros objetos.
+
+        Relação "tem-um" ou "tem-vários"
+
+        Vantagens:
+                Organização: divisão de responsabilidades,
+                Coesão,
+                Flexibilidade,
+                Reuso.
+         */
+    }
+}
