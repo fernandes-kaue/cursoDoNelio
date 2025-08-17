82ed658b37bbc2d744ec1e771732a012ab5b781a
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-17 20:51:55 -0300

    feat(ExercicioComposicao): criação de classe HoursContract

--- src/main/java/org/example/Modulo12EnumComposicao/ExercicioComposicao.java
+++ src/main/java/org/example/Modulo12EnumComposicao/ExercicioComposicao.java
@@ -1,5 +1,7 @@
 package org.example.Modulo12EnumComposicao;
 
+import java.util.Date;
+
 public class ExercicioComposicao {
     public static void main(String[] args) {
 
@@ -20,4 +22,42 @@ public class ExercicioComposicao {
             this.name = name;
         }
     }
+
+    class HourContract {
+        private Date date;
+        private Double valuePerHour;
+        private Integer hours;
+
+        public HourContract(Date date, Double valuePerHour, Integer hours) {
+            this.date = date;
+            this.valuePerHour = valuePerHour;
+            this.hours = hours;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public Double getValuePerHour() {
+            return valuePerHour;
+        }
+
+        public void setValuePerHour(Double valuePerHour) {
+            this.valuePerHour = valuePerHour;
+        }
+
+        public Integer getHours() {
+            return hours;
+        }
+
+        public void setHours(Integer hours) {
+            this.hours = hours;
+        }
+    }
+
+
 }
