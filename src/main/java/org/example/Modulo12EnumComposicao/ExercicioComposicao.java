6c478208133489e66d6440eed95237b3075e7f7c
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-17 21:01:41 -0300

    feat(ExercicioComposicao): getters/setters de Worker

--- src/main/java/org/example/Modulo12EnumComposicao/ExercicioComposicao.java
+++ src/main/java/org/example/Modulo12EnumComposicao/ExercicioComposicao.java
@@ -81,6 +81,42 @@ public class ExercicioComposicao {
             this.baseSalary = baseSalary;
             this.department = department;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public WorkerLevel getLevel() {
+            return level;
+        }
+
+        public void setLevel(WorkerLevel level) {
+            this.level = level;
+        }
+
+        public Double getBaseSalary() {
+            return baseSalary;
+        }
+
+        public void setBaseSalary(Double baseSalary) {
+            this.baseSalary = baseSalary;
+        }
+
+        public Department getDepartment() {
+            return department;
+        }
+
+        public void setDepartment(Department department) {
+            this.department = department;
+        }
+
+        public void setContracts(List<HourContract> contracts) {
+            this.contracts = contracts;
+        }
     }
 
 
