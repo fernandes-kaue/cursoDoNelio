29923a24b0503602d585dc82ae76f91442425896
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-23 22:42:24 -0300

    test(ExercicioStringBuilder): criando Post

--- src/main/java/org/example/Modulo12EnumComposicao/ExercicioStringBuilder.java
+++ src/main/java/org/example/Modulo12EnumComposicao/ExercicioStringBuilder.java
@@ -1,14 +1,22 @@
 package org.example.Modulo12EnumComposicao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class ExercicioStringBuilder {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
         Comment c1 = new Comment("Have a nice trip!");
         Comment c2 = new Comment("Wow, thats awesome!");
-        Post p1 = new Post(moment, title, content, likes);
+        Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country!",
+                12);
+
     }
 
     static class Post {
