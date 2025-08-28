9aa684e9a70d0d0b2d78d04fae6287a428323896
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-28 20:51:01 -0300

    test(Main): fazendo logica de teste na main

--- src/main/java/org/example/Modulo12EnumComposicao/ExercicioDeFixacao/Main.java
+++ src/main/java/org/example/Modulo12EnumComposicao/ExercicioDeFixacao/Main.java
@@ -1,7 +1,42 @@
 package org.example.Modulo12EnumComposicao.ExercicioDeFixacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // commit placeholder :/
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String clientName = sc.nextLine();
+        System.out.print("Email: ");
+        String clientEmail = sc.nextLine();
+        System.out.println("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.nextLine());
+
+        System.out.println("Enter order data: ");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.nextLine());
+        System.out.print("How many items to this order?");
+        int itemsQuantity = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < itemsQuantity; i++) {
+            System.out.println("Enter #" + (i + 1) + " item data:");
+            System.out.print("Product name: ");
+            String productName = sc.nextLine();
+            System.out.print("Price: ");
+            Double productPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            Integer productQuantity = sc.nextInt();
+        }
+
+
+
+
     }
 }
