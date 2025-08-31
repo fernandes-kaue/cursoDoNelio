4befe9fe4ff167b2839addfa52df919d8e3238ae
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-08-30 21:14:51 -0300

    feat(OrderItem): composicao com Product e toString()

--- src/main/java/org/example/Modulo12EnumComposicao/ExercicioDeFixacao/OrderItem.java
+++ src/main/java/org/example/Modulo12EnumComposicao/ExercicioDeFixacao/OrderItem.java
@@ -3,10 +3,12 @@ package org.example.Modulo12EnumComposicao.ExercicioDeFixacao;
 public class OrderItem {
     private Integer quantity;
     private Double price;
+    private Product product;
 
-    public OrderItem(Integer quantity, Double price) {
+    public OrderItem(Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
+        this.product = product;
     }
 
     public Integer getQuantity() {
@@ -27,5 +29,14 @@ public class OrderItem {
 
     public Double subTotal() {
         return quantity * price;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(product.getName() + ", $" + String.format("%.2f", product.getPrice()) + ", Quantity: " + quantity + ", Subtotal: $" + String.format("%.2f", subTotal()));
+
+        return sb.toString();
     }
 }
