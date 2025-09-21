bbf261c363f6753338ca09d37f6a586a77128a13
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-09-21 20:15:56 -0300

    feat(outsorcedEmployee): classe para implementar polimorfismo

---
+++ src/main/java/org/example/Modulo13HerancaPolimorfismo/Polimorfismo/ExercicioResolvido/OutsorcedEmployee.java
@@ -0,0 +1,19 @@
+package org.example.Modulo13HerancaPolimorfismo.Polimorfismo.ExercicioResolvido;
+
+public class OutsorcedEmployee extends Employee{
+    private Double additionalCharge;
+
+    public OutsorcedEmployee(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    public OutsorcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public Double payment() {
+        return super.payment() + additionalCharge + (additionalCharge * 0.1);
+    }
+}
