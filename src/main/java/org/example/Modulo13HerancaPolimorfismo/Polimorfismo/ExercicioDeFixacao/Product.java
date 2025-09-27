5df2c418aea1a3ad0a608e16e14e759b52e8999c
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-09-27 20:53:37 -0300

    fix(Product): conversão para casa decimais

--- src/main/java/org/example/Modulo13HerancaPolimorfismo/Polimorfismo/ExercicioDeFixacao/Product.java
+++ src/main/java/org/example/Modulo13HerancaPolimorfismo/Polimorfismo/ExercicioDeFixacao/Product.java
@@ -13,7 +13,7 @@ public class Product {
     }
 
     public String priceTag() {
-        return String.format("%.2f", name + " $ " + price);
+        return name + " $ " + String.format("%.2f", price);
     }
 
     public String getName() {
