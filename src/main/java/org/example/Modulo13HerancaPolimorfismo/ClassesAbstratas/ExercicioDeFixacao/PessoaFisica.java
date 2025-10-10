edce9f7b0fd649326629d2f7e57419e99506e98b
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-10-10 07:49:57 -0300

    feat(PessoaFisica): classe de PF

---
+++ src/main/java/org/example/Modulo13HerancaPolimorfismo/ClassesAbstratas/ExercicioDeFixacao/PessoaFisica.java
@@ -0,0 +1,38 @@
+package org.example.Modulo13HerancaPolimorfismo.ClassesAbstratas.ExercicioDeFixacao;
+
+public class PessoaFisica extends Contribuinte {
+    private String nome;
+    private Double rendaAnual;
+    private Double gastosComSaude;
+
+    public PessoaFisica() {
+    }
+
+    public PessoaFisica(String nome, Double rendaAnual, Double gastosComSaude) {
+        this.nome = nome;
+        this.rendaAnual = rendaAnual;
+        this.gastosComSaude = gastosComSaude;
+    }
+
+    @Override
+    public Double getIR() {
+        if (rendaAnual < 20000) {
+            return rendaAnual * 0.15;
+        } else {
+            return rendaAnual * 0.25;
+        }
+    }
+
+    @Override
+    public Double getImposto() {
+        if (gastosComSaude != null && gastosComSaude != 0) {
+            return gastosComSaude * 0.5;
+        }
+        return 0.0;
+    }
+
+    @Override
+    public Double getTotalImposto() {
+        return getIR() + getImposto();
+    }
+}
