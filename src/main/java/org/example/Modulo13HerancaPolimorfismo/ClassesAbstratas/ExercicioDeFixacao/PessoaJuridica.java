f56fb0b4b6013e50ddf6e55a08057f4df40b2317
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-10-20 23:32:54 -0300

    feat(PessoaJuridica): construtor

--- src/main/java/org/example/Modulo13HerancaPolimorfismo/ClassesAbstratas/ExercicioDeFixacao/PessoaJuridica.java
+++ src/main/java/org/example/Modulo13HerancaPolimorfismo/ClassesAbstratas/ExercicioDeFixacao/PessoaJuridica.java
@@ -1,7 +1,17 @@
+
 package org.example.Modulo13HerancaPolimorfismo.ClassesAbstratas.ExercicioDeFixacao;
 
 public class PessoaJuridica extends Contribuinte {
     private Integer numeroDeFuncionarios;
+
+    public PessoaJuridica() {
+    }
+
+    public PessoaJuridica(String nome, Double rendaAnual, Integer numeroDeFuncionarios) {
+        this.nome = nome;
+        this.rendaAnual = rendaAnual;
+        this.numeroDeFuncionarios = numeroDeFuncionarios;
+    }
 
     @Override
     public Double getIR() {
