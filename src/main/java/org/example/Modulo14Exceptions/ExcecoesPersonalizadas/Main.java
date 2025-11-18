1cbc90f7fa6d9f9299629f99c131dbd57bf2b530
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-11-17 22:53:06 -0300

    test(Main): classe para testes

---
+++ src/main/java/org/example/Modulo14Exceptions/ExcecoesPersonalizadas/Main.java
@@ -0,0 +1,26 @@
+package org.example.Modulo14Exceptions.ExcecoesPersonalizadas;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.printf("Room number: ");
+        Integer roomNumber = sc.nextInt();
+        sc.nextLine();
+        System.out.printf("Check-in date (dd/MM/yyyy): ");
+        Date checkInDate = sdf.parse(sc.nextLine());
+        System.out.printf("Check-out date (dd/MM/yyyy): ");
+        Date checkOutDate = sdf.parse(sc.nextLine());
+        sc.close();
+
+        Reservation reservation = new Reservation(roomNumber, checkInDate, checkOutDate);
+        System.out.println(reservation);
+
+    }
+}
