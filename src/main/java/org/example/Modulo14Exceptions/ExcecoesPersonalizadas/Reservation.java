7fc433caf0506ad166fb87fdc181841cb52e79b1
fernandes-kaue <fernandes-kaue@users.noreply.github.com>
2025-11-17 22:33:15 -0300

    feat(Reservation): inicio de criação de excecoes personalizadas

---
+++ src/main/java/org/example/Modulo14Exceptions/ExcecoesPersonalizadas/Reservation.java
@@ -0,0 +1,27 @@
+package org.example.Modulo14Exceptions.ExcecoesPersonalizadas;
+
+import java.util.Date;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Reservation() {}
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer duration() {
+        return (int) ((checkOut.getTime() - checkIn.getTime()) / (1000 * 60 * 60));
+    }
+
+    public void updateDates(Date newCheckInDate, Date newCheckOutDate) {
+        this.checkIn = newCheckInDate;
+        this.checkOut = newCheckOutDate;
+    }
+
+}
